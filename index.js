313006b1036e341e12c1e1f2d5ed4e35341c5f00
filodex <36601598+filodex@users.noreply.github.com>
2022-07-13 21:00:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -398,4 +398,27 @@ alert( Article.publisher ); // Илья Кантор
 alert( arr instanceof Array ); // true
 alert( arr instanceof Object ); // true
 
+//  примесь – это класс, методы которого предназначены для использования в других классах, причём без наследования от примеси.
+// Простейший способ реализовать примесь в JavaScript – это создать объект с полезными методами, которые затем могут быть легко добавлены в прототип любого класса.
+// примесь
+let sayHiMixin = {
+  sayHi() {
+    alert(`Привет, ${this.name}`);
+  },
+  sayBye() {
+    alert(`Пока, ${this.name}`);
+  }
+};
 
+// использование:
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+// копируем методы
+Object.assign(User.prototype, sayHiMixin);
+
+// теперь User может сказать Привет
+new User("Вася").sayHi(); // Привет, Вася!
